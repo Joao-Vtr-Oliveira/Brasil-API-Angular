91b9041252ec2e820ab98f1e6ac9d0c7253f1112
João V. <joao.vtr.oliveira@outlook.com>
2025-06-23 10:46:35 -0300

    chores: add jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Brasil-API-Angular');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Brasil-API-Angular');
-  });
 });
