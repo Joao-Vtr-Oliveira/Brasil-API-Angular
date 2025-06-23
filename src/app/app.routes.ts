c67eb58eedc7908f2a966a6e5bb9c8f59f94e56f
João V. <joao.vtr.oliveira@outlook.com>
2025-06-23 19:13:21 -0300

    feat: add a home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CepComponent } from './cep/cep.component';
 import { DddComponent } from './ddd/ddd.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'cep', component: CepComponent},
   {path: 'ddd', component: DddComponent}
 ];
