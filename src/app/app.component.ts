a99310d74e380323995671fd99142270edd3be0f
João V. <joao.vtr.oliveira@outlook.com>
2025-06-23 11:21:56 -0300

    feat: add a test component to test services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CepService } from './services/cep/cep.service';
 
 @Component({
-  selector: 'app-root',
-  imports: [MatCardModule, MatButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-root',
+	imports: [
+		MatCardModule,
+		MatButtonModule,
+		FormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'Brasil-API-Angular';
+	private cepService = inject(CepService);
+  cep = '';
+
+	location = this.cepService.readLocation;
+
+	onCepChange() {
+    this.cepService.fetchLocation(this.cep).subscribe();
+  }
 }
