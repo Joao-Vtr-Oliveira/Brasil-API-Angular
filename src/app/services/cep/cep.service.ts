3b0e9faee9208f29c0bc30d64bfab737a1eb31f6
João V. <joao.vtr.oliveira@outlook.com>
2025-06-23 12:15:05 -0300

    feat: add an error case

--- src/app/services/cep/cep.service.ts
+++ src/app/services/cep/cep.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { LocationCep } from './cep.model';
-import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { LocationCep, LocationError } from './cep.model';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
@@ -9,15 +9,32 @@ import { tap } from 'rxjs';
 export class CepService {
 	private httpClient = inject(HttpClient);
 	private location = signal<LocationCep | undefined>(undefined);
+	private error = signal<LocationError | null>(null);
 
 	readLocation = this.location.asReadonly();
+	readError = this.error.asReadonly();
+
+	
 
 	fetchLocation(cep: string) {
 		return this.httpClient
-			.get<LocationCep>(`https://brasilapi.com.br/api/cep/v1/${cep}`, {})
+			.get<LocationCep>(`https://brasilapi.com.br/api/cep/v2/${cep}`, {})
 			.pipe(
 				tap((locationData) => {
 					this.location.set(locationData);
+					console.log(locationData);
+				}),
+				catchError((error: HttpErrorResponse) => {
+					const parsedError: LocationError = {
+						name: error.name,
+						message: error.message,
+						type: error.error?.type ?? 'unkown',
+						errors: error.error?.errors ?? []
+					};
+
+					this.error.set(parsedError)
+
+					return throwError(() => parsedError);
 				})
 			);
 	}
