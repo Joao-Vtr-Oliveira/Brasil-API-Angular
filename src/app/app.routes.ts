5ab164a6c728c276b5c770260144b5b37e706855
João V. <joao.vtr.oliveira@outlook.com>
2025-06-23 18:30:58 -0300

    feat: add cep and ddd routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CepComponent } from './cep/cep.component';
+import { DddComponent } from './ddd/ddd.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'cep', component: CepComponent},
+  {path: 'ddd', component: DddComponent}
+];
