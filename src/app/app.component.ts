392d04e52489aff9897c40768e0b5ca02d7f4833
João V. <joao.vtr.oliveira@outlook.com>
2025-06-23 12:15:35 -0300

    feat: add a button that directs to map and a error case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	computed,
+	inject,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CepService } from './services/cep/cep.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
 	selector: 'app-root',
@@ -14,18 +20,27 @@ import { CepService } from './services/cep/cep.service';
 		FormsModule,
 		MatFormFieldModule,
 		MatInputModule,
+		MatIconModule,
 	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-	private cepService = inject(CepService);
-  cep = '';
+	cepService = inject(CepService);
+	cep = '';
 
 	location = this.cepService.readLocation;
 
+	mapsUrl = computed(
+		() =>
+			'https://www.google.com/maps?q=' +
+			this.location()?.location?.coordinates?.latitude +
+			',' +
+			this.location()?.location?.coordinates?.longitude
+	);
+
 	onCepChange() {
-    this.cepService.fetchLocation(this.cep).subscribe();
-  }
+		this.cepService.fetchLocation(this.cep).subscribe();
+	}
 }
