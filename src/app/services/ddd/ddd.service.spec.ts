6db462c6bf27d7c12737434135651391ff38511d
João V. <joao.vtr.oliveira@outlook.com>
2025-06-24 11:13:58 -0300

    test: add a fetch test in ddd

--- src/app/services/ddd/ddd.service.spec.ts
+++ src/app/services/ddd/ddd.service.spec.ts
@@ -2,25 +2,55 @@ import { TestBed } from '@angular/core/testing';
 
 import { DddService } from './ddd.service';
 import { provideHttpClient } from '@angular/common/http';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import {
+	HttpTestingController,
+	provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { DDDInterface } from './ddd.model';
 
 describe('DddService', () => {
-  let service: DddService;
+	let service: DddService;
 	let httpMock: HttpTestingController;
-  
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [DddService, provideHttpClient(), provideHttpClientTesting()],
-    });
-    service = TestBed.inject(DddService);
-    httpMock = TestBed.inject(HttpTestingController);
-  });
 
-    afterEach(() => {
-      httpMock.verify();
-    });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [DddService, provideHttpClient(), provideHttpClientTesting()],
+		});
+		service = TestBed.inject(DddService);
+		httpMock = TestBed.inject(HttpTestingController);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	afterEach(() => {
+		httpMock.verify();
+	});
+
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
+
+	it('should fetch ddd and update signals on success', (done) => {
+		const mockData: DDDInterface = {
+			state: 'RJ',
+			cities: [
+				'TERESÓPOLIS',
+				'TANGUÁ',
+				'SEROPÉDICA',
+				'SÃO JOÃO DE MERITI',
+				'SÃO GONÇALO',
+			],
+		};
+		service.fetchDdd('21').subscribe({
+			next: (data) => {
+				expect(data).toEqual(mockData);
+				expect(service.readDdd()).toEqual(mockData);
+				expect(service.readError()).toBe(null);
+        done();
+			},
+			error: done,
+		});
+    const req = httpMock.expectOne('https://brasilapi.com.br/api/ddd/v1/21');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockData);
+	});
 });
