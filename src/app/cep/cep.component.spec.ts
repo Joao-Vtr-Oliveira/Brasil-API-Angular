46eceb0b8b08cd470ed6f58c61889dbf4e3a37e2
João V. <joao.vtr.oliveira@outlook.com>
2025-06-24 14:36:12 -0300

    test: add a test case for the input

--- src/app/cep/cep.component.spec.ts
+++ src/app/cep/cep.component.spec.ts
@@ -3,24 +3,89 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CepComponent } from './cep.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { CepService } from '../services/cep/cep.service';
+import { Observable } from 'rxjs';
+import { LocationCep, LocationError } from '../services/cep/cep.model';
+import { signal } from '@angular/core';
+
+class fakeCepService {
+	private location = signal<LocationCep | undefined>(undefined);
+	private error = signal<LocationError | null>(null);
+	private loading = signal(false);
+
+	readLocation = this.location.asReadonly();
+	readError = this.error.asReadonly();
+	readLoading = this.loading.asReadonly();
+
+	mockData: LocationCep = {
+		cep: '32676048',
+		state: 'MG',
+		city: 'Betim',
+		neighborhood: 'Amarante',
+		street: 'Rua Padre Toledo',
+		service: 'open-cep',
+		location: {
+			coordinates: {
+				longitude: '-44.1388393',
+				latitude: '-19.9468875',
+			},
+		},
+	};
+	mockError: LocationError = {
+		name: 'ServiceError',
+		message: 'CEP não encontrado',
+		type: 'service',
+		errors: [],
+	};
+
+	fetchLocation(fetchCase: 'pass' | 'fail') {
+		if (fetchCase === 'pass') return this.location.set(this.mockData);
+		return this.error.set(this.mockError);
+	}
+}
 
 describe('CepComponent', () => {
-  let component: CepComponent;
-  let fixture: ComponentFixture<CepComponent>;
+	let component: CepComponent;
+	let fixture: ComponentFixture<CepComponent>;
+	let service: fakeCepService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CepComponent],
-      providers: [provideHttpClientTesting(), provideHttpClient()]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [CepComponent],
+			providers: [
+				{ provide: CepService, useClass: fakeCepService },
+				provideHttpClientTesting(),
+				provideHttpClient(),
+			],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(CepComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(CepComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+		service = TestBed.inject(CepService) as unknown as fakeCepService;
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should test the input', async () => {
+		const input: HTMLInputElement = fixture.nativeElement.querySelector(
+			'[data-testid="inputText-cepComponent"]'
+		);
+
+		input.value = '31676048';
+		input.dispatchEvent(new Event('input'));
+		fixture.detectChanges();
+
+		service.fetchLocation('pass');
+		fixture.detectChanges();
+		await fixture.whenStable();
+
+		const p = fixture.nativeElement.querySelector(
+			'[data-testid="pState-cepComponent"]'
+		) as HTMLParagraphElement;
+
+		expect(p.textContent).toContain('MG');
+	});
 });
