332be0182f7060b2c091973666752da75e644d57
João V. <joao.vtr.oliveira@outlook.com>
2025-06-25 10:39:38 -0300

    feat: add cnpj route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CepComponent } from './cep/cep.component';
 import { DddComponent } from './ddd/ddd.component';
 import { HomeComponent } from './home/home.component';
+import { CnpjComponent } from './cnpj/cnpj.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'cep', component: CepComponent},
-  {path: 'ddd', component: DddComponent}
+  {path: 'ddd', component: DddComponent},
+  {path: 'cnpj', component: CnpjComponent}
 ];
