7cb6c8691d675596cba1e934b2492512efe6d295
João V. <joao.vtr.oliveira@outlook.com>
2025-06-25 15:46:01 -0300

    feat: create the cnpj dialog

--- src/app/cnpj-dialog/cnpj-dialog.component.ts
+++ src/app/cnpj-dialog/cnpj-dialog.component.ts
@@ -1,22 +1,56 @@
-import { ChangeDetectionStrategy, Component, inject, input, Signal } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	computed,
+	inject,
+	input,
+	OnInit,
+	signal,
+	Signal,
+} from '@angular/core';
+import {
+	MAT_DIALOG_DATA,
+	MatDialogModule,
+	MatDialogRef,
+} from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { CnpjInterface } from '../services/cnpj/cnpj.model';
-import {MatChipsModule} from '@angular/material/chips';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
-
+import { CepService } from '../services/cep/cep.service';
 
 @Component({
 	standalone: true,
 	selector: 'app-confirm-dialog',
 	imports: [MatDialogModule, MatButtonModule, MatChipsModule, MatIconModule],
 	templateUrl: './cnpj-dialog.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConfirmDialogComponent {
-  cnpj = inject<Signal<CnpjInterface>>(MAT_DIALOG_DATA);
-  dialogRef = inject(MatDialogRef<ConfirmDialogComponent>);
+export class ConfirmDialogComponent implements OnInit {
+	dialogRef = inject(MatDialogRef<ConfirmDialogComponent>);
 
+	cnpj = inject(MAT_DIALOG_DATA) as Signal<CnpjInterface>;
+	readCnpj = this.cnpj();
+
+	cepService = inject(CepService);
+	location = this.cepService.readLocation;
+	mapsUrl = signal('');
+
+	ngOnInit(): void {
+		this.cepService
+			.fetchLocation(this.readCnpj.cep.toString())
+			.subscribe((loc) => {
+				if (loc?.location?.coordinates) {
+					this.mapsUrl.set(
+						'https://www.google.com/maps?q=' +
+							loc.location.coordinates.latitude +
+							',' +
+							loc.location.coordinates.longitude
+					);
+				}
+			});
+
+	}
 
 	onConfirm() {
 		this.dialogRef.close(true);
@@ -25,8 +59,4 @@ export class ConfirmDialogComponent {
 	onCancel() {
 		this.dialogRef.close(false);
 	}
-
-  onPhoneClick() {
-    
-  }
 }
