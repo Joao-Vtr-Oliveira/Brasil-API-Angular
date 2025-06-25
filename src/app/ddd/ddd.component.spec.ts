c22c4476377cd96acd718e85b628f8d1ca9c0ee4
João V. <joao.vtr.oliveira@outlook.com>
2025-06-25 08:30:28 -0300

    test: should test the input and template

--- src/app/ddd/ddd.component.spec.ts
+++ src/app/ddd/ddd.component.spec.ts
@@ -1,29 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DddComponent } from './ddd.component';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import {
+	HttpTestingController,
+	provideHttpClientTesting,
+} from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { DDDError, DDDInterface } from '../services/ddd/ddd.model';
+import { signal } from '@angular/core';
+import { DddService } from '../services/ddd/ddd.service';
+
+class fakeDddService {
+	private ddd = signal<DDDInterface | undefined>(undefined);
+	private error = signal<DDDError | null>(null);
+	private loading = signal(false);
+
+	readDdd = this.ddd.asReadonly();
+	readError = this.error.asReadonly();
+	readLoading = this.loading.asReadonly();
+
+	setLoading(value: boolean) {
+		this.loading.set(value);
+	}
+
+	mockData: DDDInterface = {
+		state: 'RJ',
+		cities: [
+			'TERESÓPOLIS',
+			'TANGUÁ',
+			'SEROPÉDICA',
+			'SÃO JOÃO DE MERITI',
+			'SÃO GONÇALO',
+		],
+	};
+	mockError: DDDError = {
+		name: 'ServiceError',
+		message: 'DDD não encontrado',
+		type: 'ddd_error',
+	};
+
+	fetchDdd(fetchCase: 'pass' | 'fail') {
+		if (fetchCase === 'pass') return this.ddd.set(this.mockData);
+		return this.error.set(this.mockError);
+	}
+}
 
 describe('DddComponent', () => {
-  let component: DddComponent;
-  let fixture: ComponentFixture<DddComponent>;
-	let httpMock: HttpTestingController;
+	let component: DddComponent;
+	let fixture: ComponentFixture<DddComponent>;
+	let service: fakeDddService;
 
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [DddComponent],
+			providers: [
+				{ provide: DddService, useClass: fakeDddService },
+				provideHttpClientTesting(),
+				provideHttpClient(),
+			],
+		}).compileComponents();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DddComponent],
-      providers: [provideHttpClientTesting(), provideHttpClient()]
-    })
-    .compileComponents();
+		fixture = TestBed.createComponent(DddComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+		service = TestBed.inject(DddService) as unknown as fakeDddService;
+	});
 
-    // httpMock = TestBed.inject(HttpTestingController);
-    fixture = TestBed.createComponent(DddComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+	it('should test the input and template', async () => {
+		const input: HTMLInputElement = fixture.nativeElement.querySelector(
+			'[data-testid="inputNumber-dddComponent"]'
+		);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		input.value = '21';
+		input.dispatchEvent(new Event('input'));
+		fixture.detectChanges();
+
+		service.fetchDdd('pass');
+		fixture.detectChanges();
+		await fixture.whenStable();
+
+		const h2 = fixture.nativeElement.querySelector(
+			'[data-testid="h2State-dddComponent"]'
+		) as HTMLHeadingElement;
+
+		expect(h2.textContent).toContain('RJ');
+	});
+
 });
